4ef4ef6c54396d9550a94ec42138704f9a14d57b
Nitesh150791 <109646228+Nitesh150791@users.noreply.github.com>
2026-03-27 15:41:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    return "Hello from Secure CICD pipeline with Jenkins and argoCD"
+
+@app.route("/health")
+def health():
+    return "ok", 200
+
+if __name__ == "__main__":
+    app.run(port = 5000, host='0.0.0.0')
+
