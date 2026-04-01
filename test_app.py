4afb4032a0bc02c801f529fd1eb7d439acf9f178
Nitesh150791 <109646228+Nitesh150791@users.noreply.github.com>
2026-04-01 14:55:53 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,11 +1,14 @@
 from app import app
 
-def test_home():
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
 
-def test_health():
+def test_home_route():
     client = app.test_client()
-    response = client.get('/health')
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello" in response.data
+
+
+def test_health_route():
+    client = app.test_client()
+    response = client.get("/health")
     assert response.status_code == 200
