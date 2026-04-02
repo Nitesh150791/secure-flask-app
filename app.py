af6180913fdac4fbdcb875686ad04ef14f49456a
Nitesh150791 <109646228+Nitesh150791@users.noreply.github.com>
2026-04-02 17:42:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-    return "Hello from Secure CICD pipeline with Jenkins and argoCD test"
+    return "Hello from Secure CICD pipeline – VERSION 25"
 
 @app.route("/health")
 def health():
